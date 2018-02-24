8b0e3e25b4826b3f0c0ec14ccb830288ff08817c
bagdik <bahdanchik@tut.by>
2018-02-24 18:52:15 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+  
+    let regexpr = /(-?\s?\d+)/g;
+    
+    let result = equation.match(regexpr);
+    let a = parseInt(result[0]);
+    let b = parseInt(result[2].replace(/\s/, ""));
+    let c = parseInt(result[3].replace(/\s/, ""));
+    
+    let d = Math.sqrt(Math.pow(b, 2) - 4*a*c);
+    let solutions = [];
+    solutions.push(Math.round((-b+d)/(2*a)));
+    solutions.push(Math.round((-b-d)/(2*a)));
+    return solutions.sort((a, b) => a - b);
 }
